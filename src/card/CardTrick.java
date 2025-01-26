acbd3ecadfab12582f0221aebbd23e4ca7b5bc5d
beingkomal <komaldoll112004@gmail.com>
2025-01-26 14:04:41 -0500

    Update CardTrick.java
    Updated CardTrick.java with lucky card logic and input validation.

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -10,10 +10,7 @@
  * Date: 2025-01-26
  */
 
-
 package card;
-
-
 
 import java.util.Random;
 import java.util.Scanner;
@@ -27,8 +24,8 @@ public class CardTrick {
 
         // Generate random cards
         for (int i = 0; i < 7; i++) {
-            values[i] = random.nextInt(13) + 1;  // Random values from 1-13
-            cardSuits[i] = suits[random.nextInt(4)];
+            values[i] = random.nextInt(13) + 1;  // Generate values between 1-13
+            cardSuits[i] = suits[random.nextInt(4)];  // Select a random suit
             System.out.println(cardSuits[i] + " " + values[i]);
         }
 
@@ -36,8 +33,16 @@ public class CardTrick {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a card value (1-13): ");
         int userValue = scanner.nextInt();
+
         System.out.print("Enter a suit (0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades): ");
         int suitIndex = scanner.nextInt();
+        
+        // Validate suit input
+        while (suitIndex < 0 || suitIndex > 3) {
+            System.out.print("Invalid suit. Enter 0=Hearts, 1=Diamonds, 2=Clubs, 3=Spades: ");
+            suitIndex = scanner.nextInt();
+        }
+
         String userSuit = suits[suitIndex];
 
         // Search for the user's card
@@ -54,6 +59,25 @@ public class CardTrick {
         } else {
             System.out.println("Sorry, your card is not in the magic hand.");
         }
+
+        // Lucky card logic
+        int luckyValue = 7;
+        String luckySuit = "Hearts";
+        boolean luckyCardFound = false;
+        for (int i = 0; i < 7; i++) {
+            if (values[i] == luckyValue && cardSuits[i].equals(luckySuit)) {
+                luckyCardFound = true;
+                break;
+            }
+        }
+
+        if (luckyCardFound) {
+            System.out.println("You found the lucky card: " + luckySuit + " " + luckyValue);
+        } else {
+            System.out.println("Sorry, the lucky card is not in the magic hand.");
+        }
+
+        // Close the scanner to avoid resource leak
+        scanner.close();
     }
 }
-
